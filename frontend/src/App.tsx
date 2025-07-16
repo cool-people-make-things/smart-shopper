912f21b2ae5d23c93a9729fcc8fb333d80943293
Josh Liuaana <jfliuaana@gmail.com>
2025-07-16 13:22:02 +1200

    feat: basic home page template created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import Home from "./components/Home";
+import { Toaster } from "@/components/retroui";
 
 function App() {
-  return <Home />;
+  return (
+    <main className="h-screen">
+      <div className="bg-gray-400 w-screen h-1/10 text-center flex items-center justify-center text-5xl">
+        I AM A HEADER
+      </div>
+      <Home />
+      <div className="bg-gray-600 w-screen h-1/10 text-center flex items-center justify-center text-5xl">
+        I AM A FOOTER
+      </div>
+      <Toaster />
+    </main>
+  );
 }
 
 export default App;
