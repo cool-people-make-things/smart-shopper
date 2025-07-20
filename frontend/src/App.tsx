804c5118a5a9e7d8da8deec790b4f034fcc64944
Emily Parkes <emilycoco@me.com>
2025-07-21 11:11:32 +1200

    feat: adds routes for home and commented routes for future main pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-default-export */
+import { Route, Routes } from "react-router";
 import { Home } from "./pages/Home";
 import { Toaster } from "@/components/retroui";
 
@@ -7,13 +9,19 @@ function App() {
       <div className="bg-gray-400 w-screen h-1/10 text-center flex items-center justify-center text-5xl">
         I AM A HEADER
       </div>
-      <Home />
+      
+      <Routes>
+        <Route index element={<Home />} />
+        {/* <Route path="browse" element={<Browse />} /> */}
+        {/* <Route path="cart" element={<Cart />} /> */}
+      </Routes>
+
       <div className="bg-gray-600 w-screen h-1/10 text-center flex items-center justify-center text-5xl">
         I AM A FOOTER
       </div>
       <Toaster />
     </main>
-  );
+  )
 }
 
 export default App;
