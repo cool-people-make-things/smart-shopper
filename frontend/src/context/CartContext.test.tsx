63b5104a60be3208297d33e8807c4fd18a9107b1
Kelly Keating <keating.kel@gmail.com>
2025-08-19 18:18:03 +1200

    update cart context test data

--- frontend/src/context/CartContext.test.tsx
+++ frontend/src/context/CartContext.test.tsx
@@ -1,7 +1,15 @@
 import { act, renderHook } from "@testing-library/react";
 import { expect, vi } from "vitest";
 
-import { fullProductDetails, preexistingCart } from "@/lib/test/fixtures/cart";
+import {
+  cartWithSingleItem,
+  emptyCart,
+  fullCart,
+  fullProductDetails,
+  partialCart,
+  partialCart_itemRemoved,
+  partialCart_quantityUpdated,
+} from "@/lib/test/fixtures/cart";
 
 import { useCart } from "./CartContext";
 import { setLocalData } from "./utils/localStorage";
@@ -19,7 +27,7 @@ describe("Given a user has an empty cart", () => {
     it("Then the cart contains that item under the correct supermarket", () => {
       const testProduct = {
         ...fullProductDetails,
-        id: "test_product_1",
+        id: "3330159",
         title: "Shampoo",
       };
 
@@ -28,7 +36,7 @@ describe("Given a user has an empty cart", () => {
         result.current.addCartItem("wls", testProduct);
       });
 
-      const itemInCart = result.current.cart.wls.test_product_1;
+      const itemInCart = result.current.cart.wls["3330159"];
 
       expect(itemInCart).toBeDefined();
       expect(itemInCart.product.title).toBe("Shampoo");
@@ -37,8 +45,7 @@ describe("Given a user has an empty cart", () => {
     it("Then the quantity of the new item is set to 1", () => {
       const testProduct = {
         ...fullProductDetails,
-        id: "test_product_2",
-        title: "Cake",
+        id: "2220732",
       };
 
       const { result } = renderComponentWithCart();
@@ -46,7 +53,7 @@ describe("Given a user has an empty cart", () => {
         result.current.addCartItem("pns", testProduct);
       });
 
-      const itemInCart = result.current.cart.pns.test_product_2;
+      const itemInCart = result.current.cart.pns["2220732"];
 
       expect(itemInCart).toBeDefined();
       expect(itemInCart.quantity).toBe(1);
@@ -55,8 +62,7 @@ describe("Given a user has an empty cart", () => {
     it("Then there is only that item in the cart", () => {
       const testProduct = {
         ...fullProductDetails,
-        id: "test_product_3",
-        title: "Flour",
+        id: "1110000",
       };
 
       const { result } = renderComponentWithCart();
@@ -64,42 +70,31 @@ describe("Given a user has an empty cart", () => {
         result.current.addCartItem("nw", testProduct);
       });
 
-      expect(result.current.cart).toEqual({
-        nw: {
-          test_product_3: {
-            product: testProduct,
-            quantity: 1,
-          },
-        },
-        pns: {},
-        wls: {},
-      });
+      expect(result.current.cart).toEqual(cartWithSingleItem);
     });
   });
 });
 
 describe("Given a user already has items in their cart", () => {
   describe("When the user clears their cart", () => {
-    it("Then all supermarkets show no items", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+    it("Then all supermarkets have no items", () => {
+      const { result } = renderComponentWithCart(fullCart);
       act(() => {
         result.current.clearCart();
       });
 
-      expect(result.current.cart).toEqual({
-        nw: {},
-        pns: {},
-        wls: {},
-      });
+      expect(result.current.cart.nw).toEqual({});
+      expect(result.current.cart.pns).toEqual({});
+      expect(result.current.cart.wls).toEqual({});
     });
   });
 
   describe("When the user adds a new item to their cart", () => {
     it("Then the item is added to the correct supermarket", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
       const testProduct = {
         ...fullProductDetails,
-        id: "new_item_1",
+        id: "1110548",
         title: "Broccoli",
       };
 
@@ -107,17 +102,17 @@ describe("Given a user already has items in their cart", () => {
         result.current.addCartItem("nw", testProduct);
       });
 
-      const itemInCart = result.current.cart.nw.new_item_1;
+      const itemInCart = result.current.cart.nw["1110548"];
 
       expect(itemInCart).toBeDefined();
       expect(itemInCart.product).toEqual(testProduct);
     });
 
     it("Then the quantity of the new item is set to 1", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
       const testProduct = {
         ...fullProductDetails,
-        id: "new_item_2",
+        id: "2220489",
         title: "Carrot",
       };
 
@@ -125,7 +120,7 @@ describe("Given a user already has items in their cart", () => {
         result.current.addCartItem("pns", testProduct);
       });
 
-      const itemInCart = result.current.cart.pns.new_item_2;
+      const itemInCart = result.current.cart.pns["2220489"];
 
       expect(itemInCart).toBeDefined();
       expect(itemInCart.product).toEqual(testProduct);
@@ -135,121 +130,85 @@ describe("Given a user already has items in their cart", () => {
 
   describe("When the user adds an item that already exists in their cart", () => {
     it("Then the quantity of the existing item is increased by 1", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
 
-      const existingItem = result.current.cart.nw.nw1;
+      const existingItem = result.current.cart.nw["1110001"];
       expect(existingItem.quantity).toBe(5);
 
       act(() => {
         result.current.addCartItem("nw", existingItem.product);
       });
 
-      const updatedItem = result.current.cart.nw.nw1;
+      const updatedItem = result.current.cart.nw["1110001"];
       expect(updatedItem.quantity).toBe(6);
     });
   });
 
   describe("When the user deletes an item from their cart", () => {
     it("Then the item is removed from the correct supermarket", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
-      expect(result.current.cart.nw.nw1).toBeDefined();
+      const { result } = renderComponentWithCart(partialCart);
+      expect(result.current.cart.nw["1110001"]).toBeDefined();
 
       act(() => {
-        result.current.removeCartItem("nw", "nw1");
+        result.current.removeCartItem("nw", "1110001");
       });
 
-      expect(result.current.cart.nw.nw1).toBeUndefined();
+      expect(result.current.cart.nw["1110001"]).toBeUndefined();
       expect(result.current.cart.nw).toEqual({});
     });
 
     it("Then only that item is removed from the cart", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
       act(() => {
-        result.current.removeCartItem("pns", "pns1");
+        result.current.removeCartItem("pns", "2220001");
       });
 
-      expect(result.current.cart).toEqual({
-        nw: {
-          nw1: {
-            product: {
-              ...fullProductDetails,
-              id: "nw1",
-            },
-            quantity: 5,
-          },
-        },
-        pns: {
-          pns2: {
-            product: {
-              ...fullProductDetails,
-              id: "pns2",
-            },
-            quantity: 1,
-          },
-        },
-        wls: {},
-      });
+      expect(result.current.cart).toEqual(partialCart_itemRemoved);
     });
   });
 
   describe("When the user updates the item quantity", () => {
     it("Then the item quantity is updated in the cart", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
       act(() => {
-        result.current.updateCartItemQuantity("pns", "pns2", 10);
+        result.current.updateCartItemQuantity("pns", "2220002", 10);
       });
 
-      const updatedItem = result.current.cart.pns.pns2;
+      const updatedItem = result.current.cart.pns["2220002"];
       expect(updatedItem.quantity).toBe(10);
     });
 
     it("Then only that item quantity is updated", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+      const { result } = renderComponentWithCart(partialCart);
       act(() => {
-        result.current.updateCartItemQuantity("pns", "pns1", 3);
+        result.current.updateCartItemQuantity("pns", "2220001", 300);
       });
 
-      expect(result.current.cart.pns.pns1.quantity).toBe(3);
+      expect(result.current.cart.pns["2220001"].quantity).toBe(300);
 
-      expect(result.current.cart).toEqual({
-        nw: {
-          nw1: {
-            product: {
-              ...fullProductDetails,
-              id: "nw1",
-            },
-            quantity: 5,
-          },
-        },
-        pns: {
-          pns1: {
-            product: {
-              ...fullProductDetails,
-              id: "pns1",
-            },
-            quantity: 3,
-          },
-          pns2: {
-            product: {
-              ...fullProductDetails,
-              id: "pns2",
-            },
-            quantity: 1,
-          },
-        },
-        wls: {},
-      });
+      expect(result.current.cart).toEqual(partialCart_quantityUpdated);
     });
+  });
 
-    it("Then the item is removed if the quantity is set to 0", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
+  describe("When the user sets the item quantity to 0", () => {
+    it("Then the item is removed", () => {
+      const { result } = renderComponentWithCart(partialCart);
       act(() => {
-        result.current.updateCartItemQuantity("nw", "nw1", 0);
+        result.current.updateCartItemQuantity("nw", "1110001", 0);
       });
 
-      const removedItem = result.current.cart.nw.nw1;
+      const removedItem = result.current.cart.nw["1110001"];
       expect(removedItem).toBeUndefined();
       expect(result.current.cart.nw).toEqual({});
+    });
+
+    it("Then only that item is removed from the cart", () => {
+      const { result } = renderComponentWithCart(partialCart);
+      act(() => {
+        result.current.updateCartItemQuantity("pns", "2220001", 0);
+      });
+
+      expect(result.current.cart).toEqual(partialCart_itemRemoved);
     });
   });
 });
@@ -264,16 +223,12 @@ describe("Given a user has not been to the site before", () => {
   describe("When the user visits the site for the first time", () => {
     it("Then an empty cart is loaded up", () => {
       const { result } = renderComponentWithCart(null);
-      expect(result.current.cart).toEqual({ nw: {}, pns: {}, wls: {} });
+      expect(result.current.cart).toEqual(emptyCart);
     });
 
     it("Then an empty cart is saved for next time", () => {
       renderComponentWithCart(null);
-      expect(setLocalData).toHaveBeenCalledWith("cart", {
-        nw: {},
-        pns: {},
-        wls: {},
-      });
+      expect(setLocalData).toHaveBeenCalledWith("cart", emptyCart);
     });
   });
 });
@@ -281,8 +236,8 @@ describe("Given a user has not been to the site before", () => {
 describe("Given a user has previously been to the site", () => {
   describe("When the user loads up the site", () => {
     it("Then their cart is restored to its previous state", () => {
-      const { result } = renderComponentWithCart(preexistingCart);
-      expect(result.current.cart).toEqual(preexistingCart);
+      const { result } = renderComponentWithCart(fullCart);
+      expect(result.current.cart).toEqual(fullCart);
     });
   });
 });
@@ -296,11 +251,7 @@ describe("Given a user changes their cart", () => {
     it("Then the changes are saved for the next visit", () => {
       const { result } = renderComponentWithCart();
       expect(setLocalData).toHaveBeenCalledTimes(1);
-      expect(setLocalData).toHaveBeenCalledWith("cart", {
-        nw: {},
-        pns: {},
-        wls: {},
-      });
+      expect(setLocalData).toHaveBeenCalledWith("cart", emptyCart);
 
       act(() => {
         result.current.addCartItem("nw", fullProductDetails);
--- frontend/src/lib/test/fixtures/cart.ts
+++ frontend/src/lib/test/fixtures/cart.ts
@@ -1,5 +1,5 @@
 export const fullProductDetails = {
-  id: "fake_id",
+  id: "0000000",
   title: "Product Name 500g",
   image: "https://example.com/product/p1.jpg",
   productPageUrl: "https://example.com/product/p1",
@@ -12,28 +12,175 @@ export const fullProductDetails = {
   promo: null,
 } as Product;
 
-export const preexistingCart = {
+// ----- CART STATES -----
+
+export const emptyCart = {
+  nw: {},
+  pns: {},
+  wls: {},
+};
+
+export const cartWithSingleItem = {
   nw: {
-    nw1: {
+    1110000: {
       product: {
         ...fullProductDetails,
-        id: "nw1",
+        id: "1110000",
+      },
+      quantity: 1,
+    },
+  },
+  pns: {},
+  wls: {},
+};
+
+export const fullCart = {
+  nw: {
+    1110111: {
+      product: {
+        ...fullProductDetails,
+        id: "1110111",
+      },
+      quantity: 50,
+    },
+    1110222: {
+      product: {
+        ...fullProductDetails,
+        id: "1110222",
+      },
+      quantity: 16,
+    },
+  },
+  pns: {
+    2220111: {
+      product: {
+        ...fullProductDetails,
+        id: "2220111",
+      },
+      quantity: 1,
+    },
+    2220222: {
+      product: {
+        ...fullProductDetails,
+        id: "2220222",
+      },
+      quantity: 13,
+    },
+    2220333: {
+      product: {
+        ...fullProductDetails,
+        id: "2220333",
+      },
+      quantity: 74,
+    },
+  },
+  wls: {
+    333011: {
+      product: {
+        ...fullProductDetails,
+        id: "333011",
+      },
+      quantity: 34,
+    },
+    3330222: {
+      product: {
+        ...fullProductDetails,
+        id: "3330222",
+      },
+      quantity: 12,
+    },
+    33303: {
+      product: {
+        ...fullProductDetails,
+        id: "33303",
+      },
+      quantity: 8,
+    },
+    3330444: {
+      product: {
+        ...fullProductDetails,
+        id: "3330444",
+      },
+      quantity: 5,
+    },
+  },
+};
+
+// ----- PARTIALLY FILLED CART & VARIANTS -----
+
+export const partialCart = {
+  nw: {
+    1110001: {
+      product: {
+        ...fullProductDetails,
+        id: "1110001",
       },
       quantity: 5,
     },
   },
   pns: {
-    pns1: {
+    2220001: {
       product: {
         ...fullProductDetails,
-        id: "pns1",
+        id: "2220001",
       },
       quantity: 12,
     },
-    pns2: {
+    2220002: {
       product: {
         ...fullProductDetails,
-        id: "pns2",
+        id: "2220002",
+      },
+      quantity: 1,
+    },
+  },
+  wls: {},
+};
+
+export const partialCart_itemRemoved = {
+  nw: {
+    1110001: {
+      product: {
+        ...fullProductDetails,
+        id: "1110001",
+      },
+      quantity: 5,
+    },
+  },
+  pns: {
+    2220002: {
+      product: {
+        ...fullProductDetails,
+        id: "2220002",
+      },
+      quantity: 1,
+    },
+  },
+  wls: {},
+};
+
+export const partialCart_quantityUpdated = {
+  nw: {
+    1110001: {
+      product: {
+        ...fullProductDetails,
+        id: "1110001",
+      },
+      quantity: 5,
+    },
+  },
+  pns: {
+    2220001: {
+      product: {
+        ...fullProductDetails,
+        id: "2220001",
+      },
+      quantity: 300,
+    },
+    2220002: {
+      product: {
+        ...fullProductDetails,
+        id: "2220002",
       },
       quantity: 1,
     },
