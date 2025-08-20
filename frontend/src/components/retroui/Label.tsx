f651ca14ef7d1eb425507e2505459ca343ae4db6
Josh Liuaana <jfliuaana@gmail.com>
2025-08-20 16:16:04 +1200

    feat: cart template created

---
+++ frontend/src/components/retroui/Label.tsx
@@ -0,0 +1,16 @@
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/retroui.utils";
+
+const labelVariants = cva(
+  "leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+);
+
+export const Label = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) => (
+  <LabelPrimitive.Root className={cn(labelVariants(), className)} {...props} />
+);
