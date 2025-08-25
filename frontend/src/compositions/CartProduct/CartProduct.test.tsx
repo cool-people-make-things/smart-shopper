ee1765b5703008f142bc2152df4b39032323e68c
Josh Liuaana <jfliuaana@gmail.com>
2025-08-26 11:18:24 +1200

    tidy up cartproduct and test fixtures

--- frontend/src/compositions/CartProduct/CartProduct.test.tsx
+++ frontend/src/compositions/CartProduct/CartProduct.test.tsx
@@ -1,39 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { axe } from "vitest-axe";
 
+import mockProduct from "@/lib/test/fixtures/cart_item.json";
 import { renderWithRouter } from "@/lib/test/renderWithRouter";
 
 import { CartProduct } from "./CartProduct";
 
-const mockProduct = {
-  quantity: 2,
-  product: {
-    id: "5263014",
-    title: "Pams Buttery Spread 500g",
-    image:
-      "https://a.fsimg.co.nz/product/retail/fan/image/400x400/5263014.png?w=384",
-    productPageUrl: "/shop/product/5263014_ea_000nw?name=pams-buttery-spread",
-    price: {
-      value: "4.29",
-      per: "ea",
-      unitPrice: "0.86",
-      unit: "100g",
-    },
-    promo: {
-      tag: "",
-      value: "3.39",
-      per: "ea",
-      unitPrice: "0.68",
-      unit: "100g",
-    },
-  },
-} as CartItem;
-
 describe("Given the user is looking at the cart page", () => {
-  describe("When the cart product component is rendered", () => {
+  describe("When the user is looking at an individual product", () => {
     it("Then it has no accessibility violations", async () => {
       const { container } = renderWithRouter(
-        <CartProduct item={mockProduct} />,
+        <CartProduct cartProductItem={mockProduct as CartItem} />,
       );
       const results = await axe(container, {
         rules: {
