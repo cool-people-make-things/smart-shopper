efcff597847f8a8d6632dd686ce055ee2ed8419e
Emily Parkes <emilycoco@me.com>
2025-08-26 17:13:40 +1200

    feat: use promo sticker on card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -4,33 +4,53 @@ import { RUICard } from "@/components/retroui";
 import { cn } from "@/lib/retroui.utils";
 
 import { CardDetails } from "../CardDetails";
+import { PromoSticker } from "../PromoSticker";
+import { formatPromoTagText } from "./utils/promoTag";
 
 export type ProductProps = {
   imgSrc: string;
   productTitle: string;
   price: string;
+  promo: Promo | null;
   store: string;
 };
 
-export function Card({ imgSrc, productTitle, price, store }: ProductProps) {
+export function Card({
+  imgSrc,
+  productTitle,
+  price,
+  promo,
+  store,
+}: ProductProps) {
   const { pathname } = useLocation();
   const isHome = pathname === "/";
+
+  const promoTag = formatPromoTagText(promo?.tag);
 
   return (
     <RUICard
       className={cn(
-        "shadow-none hover:shadow-none pt-1 pb-1",
+        "relative shadow-none hover:shadow-none pb-1 flex flex-col",
         isHome ? "w-[350px]" : "w-[80%]",
       )}
     >
-      <RUICard.Content className="py-0 flex justify-center">
+      {promoTag && <PromoSticker label={promoTag} />}
+
+      <RUICard.Content
+        className={cn("pb-4 flex justify-center", promoTag ? "pt-14" : "pt-6")}
+      >
         <img
           src={imgSrc}
           className={cn(isHome ? "w-50 h-50 " : "w-30 h-30")}
           alt={productTitle}
         />
       </RUICard.Content>
-      <CardDetails productTitle={productTitle} price={price} store={store} />
+      <CardDetails
+        productTitle={productTitle}
+        price={price}
+        promo={promo}
+        store={store}
+      />
     </RUICard>
   );
 }
