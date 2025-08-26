890ec998d8bd97425ff6547aa32008b0e43b28e2
Emily Parkes <emilycoco@me.com>
2025-08-26 17:32:15 +1200

    chore: update tests with promo property

--- frontend/src/components/Card/Card.test.tsx
+++ frontend/src/components/Card/Card.test.tsx
@@ -10,17 +10,19 @@ vi.mock("../CardDetails", () => ({
   CardDetails: () => <div data-testid="card-details">Mocked CardDetails</div>,
 }));
 
+const mockProduct = {
+  imgSrc:
+    "https://a.fsimg.co.nz/product/retail/fan/image/400x400/5023660.png?w=384",
+  productTitle: "Pams Butter",
+  price: "8.45",
+  promo: null,
+  store: "New World",
+};
+
 describe("Given a user is on the home page", () => {
   describe("When the Card component is rendered", () => {
     it("Then the user see's the correct image", () => {
-      renderWithRouter(
-        <Card
-          imgSrc="https://a.fsimg.co.nz/product/retail/fan/image/400x400/5023660.png?w=384"
-          productTitle="Pams Butter"
-          price="8.45"
-          store="New World"
-        />,
-      );
+      renderWithRouter(<Card {...mockProduct} />);
 
       const image = screen.getByAltText(/pams butter/i);
 
@@ -32,27 +34,13 @@ describe("Given a user is on the home page", () => {
     });
 
     it("Then it renders the carddetails component", () => {
-      renderWithRouter(
-        <Card
-          imgSrc="https://a.fsimg.co.nz/product/retail/fan/image/400x400/5023660.png?w=384"
-          productTitle="Pams Butter"
-          price="8.45"
-          store="New World"
-        />,
-      );
+      renderWithRouter(<Card {...mockProduct} />);
 
       expect(screen.getByTestId("card-details")).toBeInTheDocument();
     });
 
     it("Then it has no accessibility violations", async () => {
-      const { container } = renderWithRouter(
-        <Card
-          imgSrc="https://a.fsimg.co.nz/product/retail/fan/image/400x400/5023660.png?w=384"
-          productTitle="Pams Butter"
-          price="8.45"
-          store="New World"
-        />,
-      );
+      const { container } = renderWithRouter(<Card {...mockProduct} />);
       const results = await axe(container);
       expect(results).toHaveNoViolations();
     });
