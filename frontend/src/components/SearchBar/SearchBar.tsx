1a845a4098d26643bc7f2472f95e3d30226a207c
Emily Parkes <emilycoco@me.com>
2025-08-28 11:11:41 +1200

    feat: search bar functionality

---
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { Input } from "../retroui";
+import { useSearch } from "./SearchContext";
+
+export function SearchBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { query, setQuery } = useSearch();
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const value = e.currentTarget.value;
+      setQuery(value);
+
+      if (location.pathname !== "/browse") {
+        navigate(`/browse?q=${encodeURIComponent(value)}`);
+      } else {
+        navigate(`?q=${encodeURIComponent(value)}`, { replace: true });
+      }
+    }
+  };
+
+  return (
+    <Input
+      type="text"
+      defaultValue={query}
+      onKeyDown={handleKeyDown}
+      placeholder="Search products..."
+    />
+  );
+}
