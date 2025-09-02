a159a13c0b740b86a02a61fddcba8bdbd20537f9
Emily Parkes <emilycoco@me.com>
2025-09-02 15:19:17 +1200

    feat: fallback component for displaying render errors to users

---
+++ frontend/src/components/Fallback/Fallback.tsx
@@ -0,0 +1,25 @@
+import { InfoIcon } from "lucide-react";
+
+import { Alert } from "@/components/retroui/Alert";
+
+type FallbackProps = {
+  error?: Error;
+  resetErrorBoundary?: () => void;
+  message?: string;
+};
+
+export function Fallback({ error, message }: FallbackProps) {
+  const fallbackMessage = message
+    ? message
+    : "Something went wrong. Please try checking again later.";
+  return (
+    <Alert
+      status="warning"
+      className="flex items-center max-h-32 overflow-auto p-4"
+    >
+      <InfoIcon className="h-4 w-4 mr-3" />
+
+      <Alert.Title>{error?.message ?? fallbackMessage}</Alert.Title>
+    </Alert>
+  );
+}
